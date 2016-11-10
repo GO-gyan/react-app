bafc352b957731f1dcef4b3f8886479a18f877b6
Gobinda Thakur <gobinda.thakur@outlook.com>
2016-11-10 20:33:45 +0530

    Adding footer

--- client/components/App.js
+++ client/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import Footer from './pages/Footer'
 
 const styles = {
 	buttonStyle: {
@@ -17,16 +18,17 @@ const styles = {
    };
 
 class App extends Component {
-	navigate() {
-		this.props.history.pushState(null, "/");
-	}
+	
 	render() {
 		return (
+			<div>
 			<AppBar title="My App">
 				<IndexLink to="/" style={styles.linkStyle}><FlatButton label="Home" style={styles.buttonStyle}/></IndexLink>
 				<Link to="contact" style={styles.linkStyle}><FlatButton label="Contact Us" style={styles.buttonStyle}/></Link>
 				<Link to="about" style={styles.linkStyle}><FlatButton label="About Us" style={styles.buttonStyle}/></Link>
 			</AppBar>
+			<Footer />
+			</div>
 			)
 	}
 }
