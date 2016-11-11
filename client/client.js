c8aa1d57a83ecdd133f5e821dc7f206c06194a01
Gobinda Thakur <gobinda.thakur@outlook.com>
2016-11-11 18:08:58 +0530

    Adding login functionality

--- client/client.js
+++ client/client.js
@@ -2,24 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-import App from './components/App';
-import Home from './components/pages/Home';
-import ContactUs from './components/pages/ContactUs';
-import AboutUs from './components/pages/AboutUs';
-import Login from './components/pages/Login';
+import App from './components/App'
+
 
 injectTapEventPlugin();
 
 render(
 	<MuiThemeProvider>
-	<Router history={hashHistory}>
-		<Route path="/" component={App}>
-			<IndexRoute component={Home}></IndexRoute>
-			<Route path="contact" component={ContactUs}></Route>
-			<Route path="about" component={AboutUs}></Route>
-			<Route path="login" component={Login}></Route>
-		</Route>
-	</Router>
+		<App />
 	</MuiThemeProvider>, 
 	document.getElementById("app"));
